ec7a00eb5ceea027b49f1c0be052839c3e8ada06
Cosmodash <theo.github@bedafamily.com>
2021-04-24 19:24:14 -0700

    update test script and remove workspace.lua

--- test/premake5.lua
+++ test/premake5.lua
@@ -1,9 +1,23 @@
-workspacename = "test-workspace"
-startprojectname = "testproject"
-include "../scripts/workspace.lua"
+workspace "test-workspace"
+    architecture "x64"
+    targetdir "bin"
+    configurations {
+        "Debug",
+        "Release"
+    }
+    flags {
+        "MultiProcessorCompile"
+    }
+    startproject "testproject"
+    filter "system:windows"
+        defines {
+            "_CRT_SECURE_NO_WARNINGS"
+        }
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 curldir = "../test/curl"
+group "dependencies"
 include "../scripts/curl.lua"
+group ""
 project "testproject"
     location "testproject"
     kind "ConsoleApp"
